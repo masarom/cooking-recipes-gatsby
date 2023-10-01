e371e9269604478914c564a432f0abf0643f8b06
masarom <romansanmaria@gmail.com>
2023-10-01 12:14:09 +0200

    tutorial 5 Mdx blogposts map

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,31 +3,36 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = ( { data }) => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-        <ul>
-          {
-            data.allFile.nodes.map(node => (
-              <li key={node.name}>
-                {node.name}
-              </li>
-            ))
-          }
-        </ul>
+        {
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
+          ))
+        }
     </Layout>
   );
 }
 
 export const query = graphql`
-    query {
-      allFile {
-        nodes {
-        name
+  query {
+    allMdx(sort: {frontmatter: {date: DESC}}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
         }
+        id
+        excerpt
       }
     }
-  `
+  } 
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
