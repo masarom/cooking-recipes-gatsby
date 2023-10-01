9a994960bdfa0283358ec90b285f95b1660c2aed
masarom <romansanmaria@gmail.com>
2023-10-01 11:51:51 +0200

    map blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,17 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Otro Poquito(P)`,
+    title: `Otro Poquito`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-plugin-sass",
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"]
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },]
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,38 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container,  heading,
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import {
+  container,
+  heading,
   navLinks,
   navLinkItem,
-  navLinkText } from './layout.module.scss';
+  navLinkText,
+  siteTitle
+} from './layout.module.scss';
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+  query {
+  site {
+    siteMetadata {
+      title
+    }
+  }
+}
+`)
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
-          <li className={navLinkItem}><Link to="/">Home</Link></li>
-          <li><Link to="/about" className={navLinkText}>About</Link></li>
+          <li className={navLinkItem}>
+            <Link to="/">Home</Link></li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>About</Link></li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
